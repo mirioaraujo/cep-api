4f79861302388543b34e8b1949440b748b117373
Mirio <mirio@take.net>
2021-01-05 14:15:52 -0300

    Initial Commit

---
+++ ConsumerApi/ICepService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ConsumerApi
+{
+    public interface ICepService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ ConsumerApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsumerApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try 
+            {
+                var cepClient = RestService.For<ICepService>("http://viacep.com.br");
+                Console.WriteLine("Informe o cep desejado para consulta:");
+
+                string CepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Cep digitado:" + CepInformado);
+
+                var address = await cepClient.GetAddressAsync(CepInformado);
+                Console.Write($"\nLogradouro:{address.Logradouro},\nBairro:{address.Bairro},\nCidade:{address.Localidade},\nDDD:{address.Ddd},\nIBGE:{address.Ibge}");
+                Console.ReadKey();
+        }
+            catch (Exception e)
+            {
+                Console.WriteLine("Opa, Algo deu errado na consulta do CEP:" + e.Message);
+            }
+        }
+    }
+}
